2d425d2d476fc0f740b21251a681a96c2ad9580e
faftech3210 <ffaere@byupathway.edu>
2025-05-17 00:50:47 +0100

    Temple picture assignment wdd131 first try

---
+++ scriptz/temples.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const lastModified = document.getElementById("lastModified");
+    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+});
+const hbElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menulinks');
+
+hbElement.addEventListener('click', () => {
+    navElement.classList.toggle('open')
+})
